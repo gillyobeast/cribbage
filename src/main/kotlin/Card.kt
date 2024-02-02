928f357bcf274013be28cff751e53a948f7f3535
Lake Armitage <gillyobeast@gmail.com>
2024-02-02 22:31:13 +0000

    feat: card is comparable
    also move suit and value out of Card

--- src/main/kotlin/Card.kt
+++ src/main/kotlin/Card.kt
@@ -1,13 +1,25 @@
-data class Card(val value: Value, val suit: Suit) {
-    override fun toString(): String {
-        return "$value of $suit"
+data class Card(val value: Value, val suit: Suit) : Comparable<Card> {
+    override fun compareTo(other: Card): Int {
+        return if (this.value == other.value) {
+            this.suit.compareTo(other.suit)
+        } else {
+            this.value.compareTo(other.value)
+        }
     }
-    enum class Value {
-        Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
-    }
-    enum class Suit {
-        Spades, Hearts, Clubs, Diamonds
-    }
-    /* IDK if it'll be used, but it's neat! */
-    infix fun Value.of(suit: Suit): Card = Card(this, suit)
+
+    override fun toString() = "$value of $suit"
+
+
 }
+
+
+enum class Value {
+    Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+}
+
+enum class Suit {
+    Spades, Hearts, Clubs, Diamonds
+}
+
+/* IDK if it'll be used, but it's neat! */
+infix fun Value.of(suit: Suit): Card = Card(this, suit)
